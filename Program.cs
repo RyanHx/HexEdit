f604196e7b1f3e82695dfc8411151eed7e8e1ae4
RyanHx <hollick5@hotmail.com>
2020-11-17 15:30:32 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HexEdit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var failed = new Dictionary<string,string>();
+            foreach(var path in args)
+            {
+                try
+                {
+                    Stream outStream = File.Open(path, FileMode.Open);
+                    outStream.Seek(26, SeekOrigin.Begin);
+                    for(int i = 0; i < 13; i++)
+                    {
+                        outStream.WriteByte(00);
+                    }
+                    outStream.Flush();
+                }
+                catch(Exception e)
+                {
+                    failed.Add(path, $"Reason: {e.Message}");
+                }                
+            }
+
+            if(failed.Count > 0)
+            {
+                Console.WriteLine("Failed on following files:");
+                foreach(var f in failed)
+                {
+                    Console.WriteLine($"{f.Key}     {f.Value}");
+                }               
+            }
+            else
+            {
+                Console.WriteLine("Operation finished.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
